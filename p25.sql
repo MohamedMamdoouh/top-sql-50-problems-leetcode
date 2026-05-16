8e0fd9bb61c58b0c37f30221d22c5d2a86761f98
Mohamed Mamdouh <ellempy888@gmail.com>
2026-05-16 22:15:29 +0300

    Add some problems

---
+++ p25.sql
@@ -0,0 +1,14 @@
+-- Product Sales Analysis III
+
+with cte as
+(
+    select product_id , min(year) as year
+    from sales
+    group by product_id
+)
+
+select s1.product_id , s1.year as first_year , s1.quantity , s1.price
+from sales s1
+inner join cte c
+on c.product_id = s1.product_id 
+and c.year = s1.year
